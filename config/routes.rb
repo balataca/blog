68f45cfbc20839704f7b89cb495bd63b9b1007bc
Augusto Zanoni <augustorz@tutanota.com>
2021-04-13 20:06:25 -0300

    feature: adds api routes to rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  root 'articles#index'
+  namespace :api do
+    namespace :v1 do
+      get 'articles/index'
+      post 'articles/create'
+      get '/show/:id', to: 'articles#show'
+      delete '/destroy/:id', to: 'articles#destroy'
+    end
+  end
+
+  root 'home#index'
+  get '/*path' => 'home#index', constraints: lambda { |req|
+    req.path.exclude? 'rails/active_storage'
+  }
 end
