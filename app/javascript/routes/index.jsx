d508b3cc5f50ce3c3a0841ee4527a652304c41ac
Augusto Zanoni <augustorz@tutanota.com>
2021-04-14 14:45:04 -0300

    feature: adds route to create articles

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Articles from '../components/Articles';
 import Article from '../components/Article';
+import ArticleForm from '../components/ArticleForm';
 import ManageArticles from '../components/ManageArticles';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Articles} />
     <Route path="/articles/:id" component={Article} />
+    <Route path="/article/create" component={ArticleForm} />
     <Route path="/manage" component={ManageArticles} />
   </Switch>
 );
