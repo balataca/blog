82da6f65beca49c0c4111ab9878441ee31bc330c
Augusto Zanoni <augustorz@tutanota.com>
2021-04-13 21:45:56 -0300

    refactor: adds root element for react

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from '../components/App';
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
-    <App />,
-    document.body.appendChild(document.createElement('div')),
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    document.getElementById('app'),
   );
 });
