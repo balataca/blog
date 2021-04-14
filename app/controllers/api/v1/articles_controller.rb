97970ecb534e72b3f966e6830989b5054801ecf1
Augusto Zanoni <augustorz@tutanota.com>
2021-04-14 17:38:58 -0300

    feature: adds params cleaner

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -5,11 +5,27 @@ class Api::V1::ArticlesController < ApplicationController
     render json: articles
   end
 
-  def create
-    article = Article.new(article_params)
-
-    if params[:image]
+  def upload_image(article)
+    if params[:image].present?
       article.image.attach(params[:image])
+    end
+  end
+
+  def create
+    article = Article.new(cleaned_params)
+    upload_image(article)
+
+    if article.save
+      render json: article
+    else
+      render json: article.errors
+    end
+  end
+
+  def update
+    if article
+      article.update(cleaned_params)
+      upload_image(article)
     end
 
     if article.save
@@ -36,6 +52,10 @@ class Api::V1::ArticlesController < ApplicationController
 
   def article_params
     params.permit(:title, :subtitle, :body, :image)
+  end
+
+  def cleaned_params
+    article_params.select{|k, v| v.present? }
   end
 
   def article
