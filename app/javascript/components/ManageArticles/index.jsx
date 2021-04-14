e38c4b9a5487be674da8b7b3cbbff972eb5b555a
Augusto Zanoni <augustorz@tutanota.com>
2021-04-14 14:43:22 -0300

    feature: adds button to create articles

--- app/javascript/components/ManageArticles/index.jsx
+++ app/javascript/components/ManageArticles/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import ArticlesTable from './ArticlesTable';
+import { Button } from './styles';
 
-const ManageArticles = () => (
-  <div>
-    <ArticlesTable />
-  </div>
-);
+const ManageArticles = () => {
+  const history = useHistory();
+
+  const newArticle = () => {
+    history.push('/article/create');
+  };
+
+  return (
+    <div>
+      <ArticlesTable />
+      <Button onClick={newArticle}>Add Article</Button>
+    </div>
+  );
+};
 
 export default ManageArticles;
---
+++ app/javascript/components/ManageArticles/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import ButtonComponent from '../Button';
+
+export const Button = styled(ButtonComponent)`
+  margin-top: 1em;
+`;
+
+export default Button;
